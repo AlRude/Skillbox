846590392bc1a8b2e3662683688438c4e376e7b7
Alex <aaa.rudnev@gmail.com>
2022-12-19 16:43:32 +0700

    Script to test os and python version

--- 1st_lesson.py
+++ 1st_lesson.py
@@ -1,3 +1,10 @@
 import platform
 import sys
 
+info = 'OS info is \n{}\n\n Python is {} {}'.format(
+    platform.uname(), sys.version, platform.architecture())
+
+print(info)
+
+with open('os_info.txt', 'w') as file:
+    file.write(info)
