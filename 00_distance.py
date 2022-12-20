6eee8c89e90480fdd734c11550e7e3a33dd10896
Alex <aaa.rudnev@gmail.com>
2022-12-20 15:14:39 +0700

    works but not all cities measured

--- 00_distance.py
+++ 00_distance.py
@@ -23,16 +23,19 @@ for city in sites.keys():
 #print(len(cities))
 
 while counter < len(cities):
+    distances[cities[counter]] = {}
     if counter == len(cities)-1 :
         x1, y1 = sites[cities[counter]]
         x2, y2 = sites[cities[0]]
-        distance_name = str(cities[counter]+'_'+cities[0])
+        #distance_name = str(cities[counter]+'_'+cities[0])
+        distances[cities[counter]][cities[0]] = (x1 - x2) ** 2 + (y1 - y2) ** 2
     else:
         x1, y1 = sites[cities[counter]]
         x2, y2 = sites[cities[counter+1]]
-        distance_name = str(cities[counter]+'_'+cities[counter+1])
+        #distance_name = str(cities[counter]+'_'+cities[counter+1])
+        distances[str(cities[counter])][str(cities[counter+1])] = (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-    distances[distance_name] = (x1 - x2) ** 2 + (y1 - y2) ** 2
+    #distances[cities[counter]][cities[0]] = (x1 - x2) ** 2 + (y1 - y2) ** 2
     counter += 1
 
 
